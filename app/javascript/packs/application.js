90250a43580a6a2b288e7dffaa1e0a1e5582c516
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-03-10 21:11:54 -0400

    Pack tag updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+console.log("app js")
 require("channels")
+
+// CSS
+import './style.sass'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
